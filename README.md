163e52c0814cb8dad5c938449e04341eeb0b6534
sheryglitch <sheharyarahmedyou@gmail.com>
2024-07-05 13:49:30 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# photai
+## Phot AI
+
+## Getting Started
+
+### Installation
+
+1. Install dependencies:
+   ```bash
+   npm install
+   ```
+
+### Running the Project
+
+1. Start the server locally:
+   ```bash
+   npm run dev
+   ```
---
+++ config/index.js
@@ -0,0 +1,7 @@
+export const Config = {
+  PORT: 8000,
+  PHOT_AI_API_KEY: '6688288a9362d9b43ed451e8_5d0614bdd68002abe1fc_apyhitools',
+  PHOT_AI_API_URL:
+    'https://prodapi.phot.ai/external/api/v2/user_activity/object-replacer',
+  FILE_NAME: 'PhotAI'
+}
